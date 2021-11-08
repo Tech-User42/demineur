ad031227423268783417307fb053da027a8a0b4b
Tech-User42 <unadmin@unsystem.com>
2021-11-08 16:29:24 +0100

    push

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <stdout>
+#include <string>
 
+string OverWrite(int x) {
+    string s="";
+    for(int i=0;i<x;i++){s+="\b \b";}
+    return s;}
+
+int main(){   
+    cout<<"Lot's of ";
+    cout<<"stuff"<<OverWrite(5)<<"\r Test à la con"<<"\n";  
+    return(0);
+}
