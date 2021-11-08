9cce31125d8aa8d2377b93a7c751534399dc6075
Tech-User42 <unadmin@unsystem.com>
2021-11-08 17:42:07 +0100

    Version 0.1

--- Code/console.cpp
+++ Code/console.cpp
@@ -6,3 +6,16 @@ using namespace std;
 void clear_screen(){
     cout <<  "\x1B[2J\x1B[H" << "\n"; // Clear the entire screen !
 }
+
+const int limit = 10;
+
+void display_game(int tab[][], const int limit){
+    for(int i = 0 ; i < limit ; i++){
+        for(int j = 0 ; j < limit ; j++){
+        cout << tab[i][j];
+        }
+    }
+}
+int tab[10][10];
+
+display_game();
--- Code/console.h
+++ Code/console.h
@@ -1,1 +1,2 @@
 void clear_screen();
+void display_game(int tab[][],const int limit);
