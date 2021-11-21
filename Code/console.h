490ecd4cda6871fb658e947adbea645e29361e2f
Théo MARECHAL <theo.marechal0@gmail.com>
2021-11-21 23:12:37 +0100

    Régoganisation

--- Code/console.h
+++ Code/console.h
@@ -3,14 +3,15 @@
 */
 
 
-struct entry{
-        int row;
-        int col;
-    }; // Définition de la structure de la position du curseur.
+
 
 
 void clear_screen(); // Clear la console.
 void setup_screen(); // Règle la taille du terminal. (Bugué)
 
 int choice(); // Choix de l'action à effectuée  Creuser Marquer Rien.
+struct entry{
+        int row;
+        int col;
+    }; // Définition de la structure de la position du curseur.
 entry input(entry inp); // Fonction de récupération des coordonées.
